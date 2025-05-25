76743c50cdb9f3f88305ffed33507ced7abfa930
Indigochi1d <diego97523@gmail.com>
2025-05-26 04:04:25 +0900

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,5 @@
-import type { Config } from "tailwindcss";
+const config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+};
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+export default config;
