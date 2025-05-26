01c17aef2775c655b18e67a128f7580d50e7ab75
Indigochi1d <diego97523@gmail.com>
2025-05-27 05:00:41 +0900

    feat: tmdb 비동기 로직

---
+++ src/lib/tmdb.ts
@@ -0,0 +1,49 @@
+interface TMDBMovie {
+  id: number;
+  title: string;
+  poster_path: string;
+  vote_average: number;
+}
+
+interface TMDBResponse {
+  results: TMDBMovie[];
+}
+
+export interface Movie {
+  id: string;
+  title: string;
+  image: string;
+  rating: number;
+}
+
+export async function fetchTrendingMovies(): Promise<Movie[]> {
+  try {
+    const res = await fetch(
+      "https://api.themoviedb.org/3/trending/movie/week",
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+        next: { revalidate: 60 },
+      }
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch trending movies");
+    }
+
+    const data: TMDBResponse = await res.json();
+    console.log("data:", data);
+
+    return data.results.map((movie) => ({
+      id: movie.id.toString(),
+      title: movie.title,
+      image: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      rating: movie.vote_average,
+    }));
+  } catch (error) {
+    console.error("Error fetching trending movies:", error);
+    return [];
+  }
+}
